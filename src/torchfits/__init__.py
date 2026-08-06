43932c6835c73f478bd49ec60c1c7d0b072d9553
Sebastien Fabbro <sebfabbro@gmail.com>
2026-08-06 01:25:41 +0000

    release: cut 1.0.0rc5 on the 2.13 lane (release_lane --prerelease rc)

--- src/torchfits/__init__.py
+++ src/torchfits/__init__.py
@@ -15,7 +15,7 @@ import threading
 from importlib import import_module
 from typing import TYPE_CHECKING, Any
 
-__version__ = "1.0.0"
+__version__ = "1.0.0rc5"
 
 _NAMESPACES: dict[str, str] = {
     "table": "torchfits.table",
