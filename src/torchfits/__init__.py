f7693b2e43f73d700f4a7c7a5d3913eb873ff860
Sebastien Fabbro <sebfabbro@gmail.com>
2026-08-23 05:30:54 +0000

    chore(release): 1.1.0b1 on the 2.13 torch lane

--- src/torchfits/__init__.py
+++ src/torchfits/__init__.py
@@ -15,7 +15,7 @@ import threading
 from importlib import import_module
 from typing import TYPE_CHECKING, Any
 
-__version__ = "1.1.0"
+__version__ = "1.1.0b1"
 
 _NAMESPACES: dict[str, str] = {
     "table": "torchfits.table",
